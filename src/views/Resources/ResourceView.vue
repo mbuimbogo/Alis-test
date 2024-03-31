7784c040f6fb4515e39d41e61301fa0a779e8716
mbuimbogo <mbuimbogo99@gmail.com>
2024-03-31 03:40:36 +0300

    feat: add a modal for users to confirm deletion of an item

--- src/views/Resources/ResourceView.vue
+++ src/views/Resources/ResourceView.vue
@@ -1,3 +1,73 @@
+<script setup lang="ts">
+  import { computed, onMounted, ref, Ref } from 'vue';
+  import { useRoute, useRouter } from 'vue-router';
+  import { useProductStore } from '../../stores/ProductsStore';
+  import {
+    useSnackbarStore,
+    SNACKBAR_TYPE_SUCCESS,
+    SNACKBAR_TYPE_ERROR,
+  } from '../../stores/SnackbarStore';
+
+  const Router = useRouter();
+  const route = useRoute();
+  const store = useProductStore();
+  const product = ref(null);
+  const loading: Ref<boolean> = ref(true);
+  const deleteConfirmationDialog: Ref<boolean> = ref(false);
+
+  const resourceTitle = computed(() => {
+    return `Resource ${Router.currentRoute.value.params.resourceId}`;
+  });
+
+  function goBack() {
+    Router.back();
+  }
+
+  const snackbarStore = useSnackbarStore();
+
+  const deleteProduct = async () => {
+    if (!product.value || !product.value.id) {
+      return;
+    }
+
+    const productId = product.value.id;
+    const isDeleted = await store.deleteProduct(productId);
+
+    if (isDeleted) {
+      // Show success message in snackbar
+      snackbarStore.toggleSnackbar(
+        SNACKBAR_TYPE_SUCCESS,
+        'Product deleted successfully',
+      );
+      // Navigate back to the product list upon successful deletion
+      Router.push('/resources');
+      console.log(`item deleted ${productId}`);
+    } else {
+      snackbarStore.toggleSnackbar(
+        SNACKBAR_TYPE_ERROR,
+        'Failed to delete product',
+      );
+    }
+  };
+
+  function confirmDelete() {
+    deleteConfirmationDialog.value = true;
+  }
+
+  // Method to handle the actual deletion after confirmation
+  async function handleDeleteConfirmation() {
+    deleteConfirmationDialog.value = false; // Close the dialog
+    await deleteProduct(); // Call your existing deleteProduct method
+  }
+
+  onMounted(async () => {
+    loading.value = true;
+    setTimeout(() => (loading.value = false), 3000);
+    const productId = route.params.resourceId;
+    product.value = await store.getProductById(productId);
+  });
+</script>
+
 <template>
   <v-card
     class="h-100 md:h-70 w-100 overflow-y-auto justify-center align-center"
@@ -75,72 +145,39 @@
           <v-btn
             variant="tonal"
             class="mt-6"
-            @click="deleteProduct"
+            @click="confirmDelete"
             >Delete Product</v-btn
           >
         </v-col>
       </v-row>
+      <v-dialog
+        v-model="deleteConfirmationDialog"
+        persistent
+        max-width="600px"
+      >
+        <v-card>
+          <v-card-title class="text-h5">Confirm Deletion</v-card-title>
+          <v-card-text>
+            Are you sure you want to delete this product? This action cannot be
+            undone.
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue darken-1"
+              @click="deleteConfirmationDialog = false"
+              >Cancel</v-btn
+            >
+            <v-btn
+              color="red darken-1"
+              @click="handleDeleteConfirmation"
+              >Delete</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </template>
   </v-card>
 </template>
-
-<script setup lang="ts">
-  import { computed, onMounted, ref, Ref } from 'vue';
-  import { useRoute, useRouter } from 'vue-router';
-  import { useProductStore } from '../../stores/ProductsStore';
-  import {
-    useSnackbarStore,
-    SNACKBAR_TYPE_SUCCESS,
-    SNACKBAR_TYPE_ERROR,
-  } from '../../stores/SnackbarStore';
-
-  const Router = useRouter();
-  const route = useRoute();
-  const store = useProductStore();
-  const product = ref(null);
-  const loading: Ref<boolean> = ref(true);
-
-  const resourceTitle = computed(() => {
-    return `Resource ${Router.currentRoute.value.params.resourceId}`;
-  });
-
-  function goBack() {
-    Router.back();
-  }
-
-  const snackbarStore = useSnackbarStore();
-
-  const deleteProduct = async () => {
-    if (!product.value || !product.value.id) {
-      return;
-    }
-
-    const productId = product.value.id;
-    const isDeleted = await store.deleteProduct(productId);
-
-    if (isDeleted) {
-      // Show success message in snackbar
-      snackbarStore.toggleSnackbar(
-        SNACKBAR_TYPE_SUCCESS,
-        'Product deleted successfully',
-      );
-      // Navigate back to the product list upon successful deletion
-      Router.push('/resources');
-      console.log(`item deleted ${productId}`);
-    } else {
-      snackbarStore.toggleSnackbar(
-        SNACKBAR_TYPE_ERROR,
-        'Failed to delete product',
-      );
-    }
-  };
-
-  onMounted(async () => {
-    loading.value = true;
-    setTimeout(() => (loading.value = false), 3000);
-    const productId = route.params.resourceId;
-    product.value = await store.getProductById(productId);
-  });
-</script>
 
 <style scoped lang="scss"></style>
